7b7c62f7059327bae8be82223ac008520410d8f1
tomasbrazas97 <tomasbrazas@gmail.com>
2023-06-26 01:23:30 +0100

    raycast initial set up

---
+++ raycasting.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import math
+from settings import *
+
+#raycasting class
+class RayCasting:
+    def __init__(self, game):
+        self.game = game #instance of game
+
+    def ray_cast(self):
+        pass
+
+    def update(self):
+        self.ray_cast()
