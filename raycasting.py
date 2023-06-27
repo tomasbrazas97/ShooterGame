2036a460b412df6e3b1791b901006903d0e041a7
tomasbrazas97 <tomasbrazas@gmail.com>
2023-06-27 14:28:13 +0100

    remove wall fishbowl effect

--- raycasting.py
+++ raycasting.py
@@ -57,6 +57,9 @@ class RayCasting:
             else:
                 depth = depth_hor
 
+            # remove fishbowl effect
+            depth *= math.cos(self.game.player.angle - ray_angle)
+            
             # projection
             proj_height = SCREEN_DIST / (depth + 0.0001)
 
